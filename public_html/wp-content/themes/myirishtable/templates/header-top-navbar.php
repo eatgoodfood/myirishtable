d5358dd9f5cc6e913fa4117fa5512f3bf0a51950
Stephen Chapman <stephen@stephen-chapman.com>
2013-10-03 21:18:42 -0400

    Insert nav collapse button

--- public_html/wp-content/themes/myirishtable/templates/header-top-navbar.php
+++ public_html/wp-content/themes/myirishtable/templates/header-top-navbar.php
@@ -8,13 +8,26 @@
 	              <h1 class="jumbo"><span class="typeb">My</span> <span class="typea">Irish Table</span></h1>
 	              <p class="jumbo"><span class="typea">Recipes from the Homeland and Restaurant Eve</span></p>
     </div>
+   </div>
+   
+    <nav class="navbar navbar-default" role="navigation">
 
-    <nav role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-justified'));
-        endif;
-      ?>
-    </nav>
-  </div>
+		<div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		</div>
+
+	    <div class="collapse navbar-collapse" role="navigation">
+	      <?php
+	        if (has_nav_menu('primary_navigation')) :
+	          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-justified'));
+	        endif;
+	      ?>
+	    </div>
+  	</nav>
+
 </header>
