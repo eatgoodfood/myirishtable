3e24d016cc5e5da818f5b2786c8d04c74ee34d9a
Stephen Chapman <stephen@stephen-chapman.com>
2013-10-11 17:44:25 -0400

    edit header file and create new one

--- public_html/wp-content/themes/myirishtable/templates/header-top-navbar-slide.php
+++ public_html/wp-content/themes/myirishtable/templates/header-top-navbar-slide.php
@@ -29,5 +29,34 @@
 	      ?>
 	    </div>
   	</nav>
+  	
+  	<!-- Carousel
+    ================================================== -->
+    <div class="hidden-xs">
+	    <div id="myCarousel" class="carousel slide" data-interval="5000">
+	      <!-- Indicators -->
+	      <ol class="carousel-indicators">
+	        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	        <li data-target="#myCarousel" data-slide-to="1"></li>
+	        <li data-target="#myCarousel" data-slide-to="2"></li>
+	      </ol>
+	      <div class="carousel-inner">
+	        <div class="item active">
+	          <img src="/assets/img/carousel1.jpg" alt="First slide">
+	          
+	        </div>
+	        <div class="item">
+	          <img src="/assets/img/carousel2.jpg" alt="Second slide">
+	          
+	        </div>
+	        <div class="item">
+	          <img src="/assets/img/carousel3.jpg" alt="Third slide">
+	          
+	        </div>
+	      </div>
+	      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	    </div><!-- /.carousel -->
+	</div><!-- /.carousel-container -->
 
 </header>
