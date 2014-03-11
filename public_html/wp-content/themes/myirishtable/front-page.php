e861146275d1ed01347658857633775f38bb3775
Stephen Chapman <stephen@stephen-chapman.com>
2014-03-11 16:32:35 -0400

    add title to twitter, add data-chrome attributes, remove border on instagram pic

--- public_html/wp-content/themes/myirishtable/front-page.php
+++ public_html/wp-content/themes/myirishtable/front-page.php
@@ -2,7 +2,8 @@
  	
 		<div class="col-sm-4">
 			<div class="box">
-				<a class="twitter-timeline" href="https://twitter.com/MyIrishTable" data-widget-id="443462236623761408">Tweets by @MyIrishTable</a>
+				<h3 class="call-out">Twitter</h3>
+				<a class="twitter-timeline" href="https://twitter.com/MyIrishTable" data-widget-id="443462236623761408 data-chrome="noborders transparent">Tweets by @MyIrishTable</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 		</div>
--- public_html/wp-content/themes/myirishtable/lib/scripts.php
+++ public_html/wp-content/themes/myirishtable/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd676f372d340229a3ae0c3001e661765');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c52be430530f4b5005687fd7808e83bf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
