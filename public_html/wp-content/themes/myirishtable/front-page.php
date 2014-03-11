1f3ab86307d96a673317fa7f6cbec0d6407690d8
Stephen Chapman <stephen@stephen-chapman.com>
2014-03-11 15:09:28 -0400

    twitter widget added

--- public_html/wp-content/themes/myirishtable/front-page.php
+++ public_html/wp-content/themes/myirishtable/front-page.php
@@ -2,12 +2,8 @@
  	
 		<div class="col-sm-4">
 			<div class="box">
-				<h3 class="call-out">March 11, 2014</h3>
-				<ul class="marketing-list">
-					<li>130 recipes showcasing modern Irish fare</li>
-					<li>Debut cookbook from Cathal Armstrong</li>
-					<li>A journey from Dublin to Washington, D.C.</li>
-				</ul>
+				<a class="twitter-timeline" href="https://twitter.com/MyIrishTable" data-widget-id="443462236623761408">Tweets by @MyIrishTable</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 		</div>
 		
