72c602bce8e32c253e5d1e614d982b47289e0d3c
Stephen Chapman <stevochapman@gmail.com>
2014-03-24 10:21:11 -0400

    another attempt to fix phone size display

--- public_html/wp-content/themes/myirishtable/lib/scripts.php
+++ public_html/wp-content/themes/myirishtable/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f58b7f2642c7644c6470a40e1dc1d351');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '736cc00a65a9ea010b4f3d318f8fac3f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
