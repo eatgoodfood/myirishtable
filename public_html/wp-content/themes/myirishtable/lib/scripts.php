57185d96cd5bbf1f287f9c35337c062c6cfc7065
Stephen Chapman <stevochapman@gmail.com>
2014-03-24 11:06:10 -0400

    fix phone display with new bootstrap defaults

--- public_html/wp-content/themes/myirishtable/lib/scripts.php
+++ public_html/wp-content/themes/myirishtable/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '736cc00a65a9ea010b4f3d318f8fac3f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9009e857f5c7d0cea2e0f6e5f999458a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
